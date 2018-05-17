be8e6b6ab8a5b7a034f4158b7ad20b70d48ca58e
kznrluk <kznr.luk@gmail.com>
2018-05-17 22:39:59 +0900

    init

---
+++ twatch.js
@@ -0,0 +1,13 @@
+const twitter = require('twitter');
+
+const config = require('./config.json')
+const DIR_NAME = config.dir;
+
+if(config.twitter){
+    // Setting up Twitter API keys.
+}
+
+const result = [];
+for(directory of DIR_NAME){
+    
+}
