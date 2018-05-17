c3fb9d1a57ca8c6a4794e93ae4d79c7f3fcb8ba5
kznrluk <kznr.luk@gmail.com>
2018-05-18 00:17:27 +0900

    v1.0.0

--- twatch.js
+++ twatch.js
@@ -1,13 +1,47 @@
-const twitter = require('twitter');
+const fs = require('fs');
+const Twitter = require('twitter');
 
 const config = require('./config.json')
 const DIR_NAME = config.dir;
 
-if(config.twitter){
-    // Setting up Twitter API keys.
+// Setting up Twitter API keys.
+const client = new Twitter({
+    consumer_key : config.twitter.apiKey,
+    consumer_secret : config.twitter.apiSecret,
+    access_token_key: config.twitter.accessToken,
+    access_token_secret: config.twitter.accessSecret
+});
+
+const isDirectryHasSomeFile = async (dirName) => {
+    let result;
+    try {
+        result = await new Promise((resolve, reject) => {
+            fs.readdir(dirName, (err, files) => {
+                if(err) reject(err);
+                resolve( files.length >= 1 ? true : false );
+            })
+        });
+    } catch (err) {
+        console.log(err);
+    }
+    return result;
 }
 
-const result = [];
+const promises = [];
 for(directory of DIR_NAME){
-    
+    promises.push(isDirectryHasSomeFile(directory));
 }
+
+const noFileDirectories = [];
+Promise.all(promises)
+    .then(results => {
+        results.forEach((result, index) => {
+            if(!result){
+                noFileDirectories.push(DIR_NAME[index])
+            }
+        });
+        const tweet = `@${config.twitter.user} [twatch.js : No file directory] ${noFileDirectories}`
+        client.post('statuses/update', {status: tweet}, err => {
+            if (err) throw err;
+        });
+    });
